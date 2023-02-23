1bcd933c2ee2247dbc23248948068f1a1294ffb6
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2023-02-23 21:23:29 +0600

    dislay data in html using fetch api

--- js/index.js
+++ js/index.js
@@ -8,8 +8,16 @@ function loadUser(){
 }
 
 function displayUsers(data){
+    // for(const user of data){
+    //     console.log(user.email, user.name);
+    // }
+
+    const ul = document.getElementById('users-list');
     for(const user of data){
-        console.log(user.email, user.name);
+        console.log(user.name);
+        const li =document.createElement('li');
+        li.innerText = user.name;
+        ul.appendChild(li);
     }
     
 }
