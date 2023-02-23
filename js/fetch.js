a1174bf30136d14b62f19047ad5fe8e7eb5daeb1
Tareq_Ibna_Rahman <tirahman105@gmail.com>
2023-02-23 20:49:06 +0600

    fetch data from api

---
+++ js/fetch.js
@@ -0,0 +1,13 @@
+// console.log("connected");
+
+/* fetch('https://jsonplaceholder.typicode.com/todos/1')
+      .then(response => response.json())  // . json is not similar but close to JSON.parse
+      .then(json => console.log(json)) */
+
+const url = 'https://jsonplaceholder.typicode.com/todos/1';
+      function loadData(){
+        
+        fetch(url)
+        .then(response => response.json() )
+        .then(json => console.log(json))
+      }
